0491eab0f82dcc27dd578a493104679c6f833eba
Soshi Tsunashima <22724971+hellososhi@users.noreply.github.com>
2022-06-27 19:15:48 +0900

    bug modified

--- diccionario/main.js
+++ diccionario/main.js
@@ -1,9 +1,9 @@
 function searchEsJa() {
     let word = document.getElementById("esjaword").value
-    window.open("https://www.google.com/search?q=site%3Akotobank.jp%2Fesjaword+" + str)
+    window.open("https://www.google.com/search?q=site%3Akotobank.jp%2Fesjaword+" + word)
 }
 
 function searchJaEs() {
     let word = document.getElementById("jaesword").value
-    window.open("https://www.google.com/search?q=site%3Akotobank.jp%2Fjaesword+" + str)
+    window.open("https://www.google.com/search?q=site%3Akotobank.jp%2Fjaesword+" + word)
 }
